591fae076fe31252bb3c4a529cde7163336fe47e
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-03-02 23:33:18 +0100

    Add player.js with its class and methods

---
+++ modules/player.js
@@ -0,0 +1,30 @@
+import Gameboard from './gameboard.js';
+
+export default class Player {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+        this.alreadyHitField = [];
+    }
+
+
+    attack(cordX, cordY, gameboard) {
+        if (this.alreadyHitField.some((field) => field[0] === cordX && field[1] === cordY)) return;
+
+        this.alreadyHitField.push([cordX, cordY]);
+        gameboard.receiveAttack(cordX, cordY);
+    }
+
+    randomAttack(gameboard) {
+        if (this.alreadyHitField.length === 100) return;
+
+        let cordX;
+        let cordY;
+        do {
+            cordX = Math.floor(Math.random() * 10);
+            cordY = Math.floor(Math.random() * 10);
+        } while (this.alreadyHitField.some((field) => field[0] === cordX && field[1] === cordY));
+        gameboard.attack(cordX, cordY);
+        this.alreadyHitField.push([cordX, cordY]);
+    }
+}
